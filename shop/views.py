8d13d2171c1a3def3928d5726a1db8b1180552af
AlexeyGudvill <agoodwill04@gmail.com>
2025-02-05 00:42:43 +0500

    commit 4

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from django.core.mail import send_mail
 
 
-def category_list(request, category_slug=None):
+def category_list(request, category_slug=None): # страница категорий
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
@@ -16,38 +16,42 @@ def category_list(request, category_slug=None):
     return render(request,
                   'shop/product/index.html', {'products': products, 'category': category, 'categories': categories})
 
-
-def product_list(request, category_slug=None):
+def product_list(request, category_slug=None): # товары по категориям + поиск + сортировка по параметру
     category = None
     categories = Category.objects.all()
+    query = request.GET.get('query', '').strip()
+    sort = request.GET.get('sort')
     products = Product.objects.filter(available=True)
-    if category_slug:
+
+    if category_slug: # Фильтрация по категории
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request,
-                  'shop/product/list.html', {'products': products, 'category': category, 'categories': categories})
+
+    if query: # Фильтрация по поиску
+        products = products.filter(name__icontains=query.capitalize())
+
+    # Применение сортировки
+    if sort == 'name':
+        products = products.order_by('name')
+    elif sort == 'price':
+        products = products.order_by('price')
+    elif sort == 'stock':
+        products = products.order_by('-stock')
+    return render(request, 'shop/product/list.html', {'products': products, 'category': category, 'categories': categories, 'query': query, 'sort': sort})
 
 
-def product_detail(request, id, slug):
+def product_detail(request, id, slug): # описание товара
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request,
                   'shop/product/detail.html',
                   {'product': product})
 
 
-def product_search(request):
-    query = request.GET.get('query', '').strip()
-    products = Product.objects.filter(available=True)
-    if query:
-        products = products.filter(name__icontains=query.capitalize())  # Фильтр по вхождению строки в название
-    return render(request, 'shop/product/list.html', {'products': products, 'query': query})
-
-
-def registration(request):
+def registration(request): # страница регистрации
     return render(request, 'shop/registration.html')
 
               
-def login_register(request):
+def login_register(request): # регистрация
     if request.method == 'POST':
         email = request.POST['email']
         full_name = request.POST['full_name']
@@ -69,7 +73,8 @@ def login_register(request):
 
     return render(request, 'shop/registration.html')
 
-def login_view(request):
+
+def login_view(request): # вход
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
@@ -91,12 +96,14 @@ def login_view(request):
 
     return render(request, 'shop/registration.html')
 
-def logout_view(request):
+
+def logout_view(request): # выход из аккаунта
     request.session.flush()  # Удаляем сессию
     messages.success(request, "Вы вышли из системы.")
     return redirect('shop:registration')
 
-def profil(request):
+
+def profil(request): # страница профиля
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
@@ -107,7 +114,8 @@ def profil(request):
     ordered_orders = orders.filter(status="Заказан")
     return render(request, 'shop/profil.html', {'user': user, 'pending_orders': pending_orders, 'ordered_orders': ordered_orders, 'cart': cart})
 
-def cart(request):
+
+def cart(request): # страница корзины
     if 'user_id' not in request.session:
         return redirect('shop:registration')
     
@@ -115,7 +123,8 @@ def cart(request):
     cart = user.cart  # Корзина пользователя
     return render(request, 'shop/cart.html', {'user': user, 'cart': cart})
 
-def order(request):
+
+def order(request): # страница заказов
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
@@ -123,31 +132,23 @@ def order(request):
     orders = user.orders.all()  # Все заказы пользователя
     pending_orders = orders.filter(status="Ожидает обработки")
     ordered_orders = orders.filter(status="Заказан")
+    return render(request, 'shop/order.html', {'user': user, 'orders': orders, 'pending_orders': pending_orders, 'ordered_orders': ordered_orders})
 
-    return render(request, 'shop/order.html', {
-        'user': user,
-        'orders': orders,
-        'pending_orders': pending_orders,
-        'ordered_orders': ordered_orders
-    })
 
-def add_to_cart_or_order(request, product_id):
+def add_to_cart_or_order(request, product_id): # действия с товарами: добавить в корзину или заказать
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
     user = ShopUser.objects.get(id=request.session['user_id'])
     product = get_object_or_404(Product, id=product_id)
-
     action = request.POST.get('action', 'cart')  # Проверяем, что за действие: добавить в корзину или заказать
 
-    if action == 'order':
-        # Логика оформления заказа напрямую
+    if action == 'order': # Логика оформления заказа напрямую
         order = Order.objects.create(user=user)
         OrderItem.objects.create(order=order, product=product, quantity=1)
-        return redirect('shop:order')  # Перенаправляем на страницу заказов
+        return redirect('shop:order')
     
-    elif action == 'cart':
-        # Логика добавления в корзину
+    elif action == 'cart': # Логика добавления в корзину
         cart = user.cart
         item, created = CartItem.objects.get_or_create(cart=cart, product=product)
         item.quantity += 1
@@ -156,7 +157,8 @@ def add_to_cart_or_order(request, product_id):
 
     return redirect('shop:cart')
 
-def cart_action(request):
+
+def cart_action(request): # действия с товарами в корзине: удалить или заказать
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
@@ -166,19 +168,16 @@ def cart_action(request):
         user = ShopUser.objects.get(id=request.session['user_id'])
         cart = user.cart
 
-        if action == "order":
-            # Логика оформления заказа
+        if action == "order":  # Логика оформления заказа
             if selected_items:
                 order = Order.objects.create(user=user)
                 for item_id in selected_items:
                     cart_item = get_object_or_404(CartItem, id=item_id, cart=cart)
                     OrderItem.objects.create(order=order, product=cart_item.product, quantity=cart_item.quantity)
-                # Удаляем оформленные товары из корзины
-                CartItem.objects.filter(id__in=selected_items).delete()
+                CartItem.objects.filter(id__in=selected_items).delete() # Удаляем оформленные товары из корзины
             return redirect('shop:order')
         
-        elif action == "remove":
-            # Логика удаления товаров
+        elif action == "remove": # Логика удаления товаров
             for item_id in selected_items:
                 cart_item = get_object_or_404(CartItem, id=item_id, cart=cart)
                 if cart_item.quantity > 1:
@@ -190,8 +189,8 @@ def cart_action(request):
 
     return redirect('shop:cart')
 
-# Удаление заказа
-def remove_order(request, order_id):
+
+def remove_order(request, order_id): # удаление заказа
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
@@ -199,11 +198,12 @@ def remove_order(request, order_id):
     order = get_object_or_404(Order, id=order_id, user=user)
 
     if order.status == "Ожидает обработки":
-        order.delete()  # Удаляем заказ, если его статус "Ожидает обработки"
+        order.delete()
     
-    return redirect('shop:order')  # Перенаправление на страницу с заказами
+    return redirect('shop:order') 
 
-def order_payment(request, order_id):
+
+def order_payment(request, order_id): # заказ товара - оплата
     if 'user_id' not in request.session:
         return redirect('shop:registration')
 
@@ -212,7 +212,6 @@ def order_payment(request, order_id):
 
     if request.method == "POST":
         # Симуляция оплаты (можно интегрировать платежный шлюз)
-        
         order.set_status_ordered()
 
         # Отправка письма
